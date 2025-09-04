96d358408eec78cb8c50321d336c7bbdcfcb0d00
Calcasmy <technoatomic@gmail.com>
2025-09-03 20:49:32 -0400

    created a bash script to fetch from git, reset origin and cycle the service

---
+++ update_vivarium.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+git fetch origin main && git reset --hard origin/main
+sudo systemctl stop vivarium.service
+sudo systemctl disable vivarium.service
+sudo systemctl daemon-reload
+sudo systemctl enable vivarium.service
+sudo systemctl start vivarium.service
+sudo journalctl -u vivarium.service -f
